5c7103976566433a1a0437319a93646d60d2b819
XaMeLeoH907 <xameleoh907@gmail.com>
2024-10-07 10:21:15 +0300

    module_3_1

---
+++ .venv/module_3_1.py
@@ -0,0 +1,29 @@
+# Счётчик вызовов
+def count_calls (): # счетчик вызова фукций
+    global \
+        calls
+    calls += 1 # увеличиваем глабальную переменную каждый раз при вызове функции
+
+
+def string_info(string_f): # функция сбора сведений строки
+    count_calls() # вызов счетчика функций
+    str_info = (len(string_f), string_f.upper(), string_f.lower()) # создание кортежа с данными
+    return str_info # возвращаем кортеж
+
+
+def is_contains(string, list_): # функция поиска строки в списке
+    count_calls() # вызов счетчика функций
+    for i in list_: # перебор строки
+        if i.upper() ==  string.upper(): # поик совпадений
+            is_contains_f = True
+        else:
+            is_contains_f = False
+    return is_contains_f # возвращаем результат поиска
+
+
+calls = 0
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
