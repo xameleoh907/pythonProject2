7b8c3c5dc80028328446c7b87f0163dcbebb34ab
XaMeLeoH907 <xameleoh907@gmail.com>
2024-10-10 17:43:23 +0300

    module_3_5_v2

--- .venv/module_3_5.py
+++ .venv/module_3_5.py
@@ -18,8 +18,6 @@ def get_multiplied_digits(number):
     first = int(str_number[0])  # присваиваем first первый элемент
     if len(str_number) > 1: # если длина строки больше 1
         return first * get_multiplied_digits(int(str_number[1:])) # возвращаем
-    elif int(str_number[0]) == 0:
-        return 1
     else:
         return int(str_number[0]) # иначе возвращаем оставшийся элемент строки
 
