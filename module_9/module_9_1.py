7524a4827c44339a15285d76a69dfae044fa5969
XaMeLeoH907 <xameleoh907@gmail.com>
2024-11-29 00:14:18 +0300

    module_9_1

---
+++ module_9/module_9_1.py
@@ -0,0 +1,17 @@
+# "Вызов разом"
+
+
+def apply_all_func(int_list, *functions):
+    result = {}
+    for function in functions:
+        result[function.__name__] =  function(int_list)
+    return result
+
+
+# Пример работы кода:
+try:
+    print(apply_all_func([6, 20, 15, 9], max, min))
+    print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+    print(apply_all_func([б, 20, 15, 9], max, min))
+except NameError as exc:
+    print(f'Введены недопустимые данные: {exc}')
